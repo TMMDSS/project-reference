4e3d11e90a38f6a4dd4eeae2df7b84b0951b73c8
TMMDSS <souzatailam@gmail.com>
2023-06-26 14:25:31 -0300

    banner completo adicionado

---
+++ script.js
@@ -0,0 +1,19 @@
+// B A N N E R
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 3000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
