493d40fd62cd5d1353ba61912603aeae00827b96
TMMDSS <souzatailam@gmail.com>
2023-06-28 21:37:21 -0300

    comecei a mexer no javaScript das redes sociais

--- script.js
+++ script.js
@@ -17,3 +17,52 @@ function proximaImg(){
 
     document.getElementById('radio'+cont).checked = true
 }
+
+// S O C I A L
+let inst = [...document.querySelectorAll(".imgFooter")]
+inst.map((el, chave) => {
+    el.addEventListener("mouseover",() => {
+        const entrar = () => {
+            if (el.src.match("img/instagramBlack.png")) {
+            el.src = "img/instagramColor.png";
+            } else {
+            el.src = "img/instagramBlack.png";
+            }
+        }
+
+
+    entrar()
+    })
+    el.addEventListener("mouseout",() => {
+        const sair = () => {
+            if (el.src.match("img/instagramColor.png")) {
+            el.src = "img/instagramBlack.png";
+            } else {
+            el.src = "img/instagramBlack.png";
+            }
+        }
+
+
+        sair()
+    })
+    
+})
+
+
+// function entrar() {
+//     var imagem = document.getElementById("logoLaranja");
+//     if (imagem.src.match("imagens/crunchyroll-logo.png")) {
+//       imagem.src = "imagens/crunchyroll-logo-branco.png";
+//     } else {
+//       imagem.src = "imagens/crunchyroll-logo.png";
+//     }
+//   }
+  
+//   function sair() {
+//       var imagem = document.getElementById("logoLaranja");
+//       if (imagem.src.match("imagens/crunchyroll-logo.png")) {
+//         imagem.src = "imagens/crunchyroll-logo-branco.png";
+//       } else {
+//         imagem.src = "imagens/crunchyroll-logo.png";
+//       }
+//     }
